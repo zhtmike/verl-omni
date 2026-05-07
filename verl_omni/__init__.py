ae92e2f8e96e9c89e99925cdd403d9e3bca515b0
Mike Cheung <zhtmike@gmail.com>
2026-05-07 10:45:54 +0800

    fix

--- verl_omni/__init__.py
+++ verl_omni/__init__.py
@@ -22,3 +22,5 @@ import verl_omni.pipelines  # noqa: E402, F401
 import verl_omni.reward_loop  # noqa: E402, F401
 import verl_omni.workers.engine  # noqa: E402, F401
 import verl_omni.workers.rollout  # noqa: E402, F401
+
+print("test")
